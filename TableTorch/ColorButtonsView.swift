20a87b2d45e8713490bde7ede2484a88e6c7619c
Brian Fuller <brianfullerusa@gmail.com>
2025-10-07 21:06:12 -0600

    Dim inactive color presets and update localization

--- TableTorch/ColorButtonsView.swift
+++ TableTorch/ColorButtonsView.swift
@@ -19,45 +19,19 @@ struct ColorButtonsView: View {
                 let isSelected = (selectedIndex == index)
 
                 Button {
-                    // Update which “button” is selected
                     selectedIndex = index
                 } label: {
-                    if isSelected {
-                        // SELECTED: Torch icon filled with the color
-                        Image(systemName: "flame.fill") // or "flashlight.on.fill"
-                            .resizable()
-                            .scaledToFit()
-                            .foregroundColor(color)
-                            .frame(width: 40, height: 40)
-                    } else {
-                        // UNSELECTED: Torch icon primarily black with a colored outline
-                        ZStack {
-                            // Base icon in black
-                            Image(systemName: "flame") //or "flashlight.off.fill
-                                .resizable()
-                                .scaledToFit()
-                                .foregroundColor(.black)
-                                .frame(width: 40, height: 40)
-
-                            // Overlaid icon that “bleeds” around edges to create a color outline
-                            Image(systemName: "flame")
-                                .resizable()
-                                .scaledToFit()
-                                .foregroundColor(color)
-                                .frame(width: 40, height: 40)
-                                // The mask trick tries to show only a subtle outline
-                                .mask(
-                                    Image(systemName: "flame")
-                                        .resizable()
-                                        .scaledToFit()
-                                        .blur(radius: 1.0)
-                                )
-                        }
-                    }
+                    Image(systemName: isSelected ? "flame.fill" : "flame")
+                        .resizable()
+                        .scaledToFit()
+                        .foregroundColor(color)
+                        .frame(width: 40, height: 40)
+                        .opacity(isSelected ? 1.0 : 0.5)
+                        .accessibilityLabel("Color \(index + 1)")
+                        .accessibilityAddTraits(isSelected ? .isSelected : [])
                 }
             }
         }
     }
 }
-
 
